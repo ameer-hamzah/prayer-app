9d2e6f6440e72f381439c4d34147d9e15b45d0c6
ameer-hamzah <ameer74860@gmail.com>
2026-01-13 18:14:46 +0500

    app fix respond

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from datetime import datetime
 from hijri_converter import Gregorian
+import os
 
 app = Flask(__name__)
 app.secret_key = "madina-marriage-hall-mosque"
@@ -68,4 +69,5 @@ def index():
 
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
